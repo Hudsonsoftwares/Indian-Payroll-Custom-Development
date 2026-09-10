528d642135f52b301be680b137bcb020f3629e6b
Hudson Software Solutions <support@hudsonsoftwares.com>
2026-09-10 09:44:56 +0530

    unnnesasry removel of files done

--- hudson_payroll_core/models/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from . import hr_rule_parameter
-from . import hr_payslip_input_section
-from . import hr_payslip_input_type
-from . import hr_payslip_input
-from . import hr_payroll_structure_type
-from . import hr_payroll_structure
-from . import hr_version
--- hudson_payroll_payrun/models/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-from . import hr_employee_type
-from . import hr_payslip
-from . import hr_payslip_run_dep_summary
-from . import payment_advice
-from . import hr_payslip_run
